f98abe730019f284ece0297420a01bbe66096300
Kesar254 <keksar122@gmail.com>
2026-07-08 11:03:52 +0300

    constant iota

---
+++ main4.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type weekday int
+
+const (
+	sunday weekday = iota
+	monday
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+)
+
+func main() {
+fmt.Println(wednesday)
+
+}
